da8be5ed052d6fa5e4d60f32df2b24a5025cff8e
David Gros <dngros@live.com>
2016-09-13 05:02:53 -0500

    add test cases. Clean code

--- assign/wormholes.py
+++ assign/wormholes.py
@@ -1,12 +1,9 @@
 from collections import namedtuple
-cases = int(raw_input())
-for c in range(cases):
-    planetCount, wormCount = map(int, raw_input().split())
+for case in range(int(raw_input())):
+    planetCount, wormCount = map(int, raw_input().rstrip().split())
     dist = [0] + [9e9]*(planetCount-1) # planet 1 source
-    wormHoles = []
     WormHole = namedtuple("WormHole", ["source","dest","cost"])
-    for w in range(wormCount):
-        wormHoles.append(WormHole( *map(int, raw_input().split() )))
+    wormHoles = [WormHole(*map(int, raw_input().split())) for w in range(wormCount)]
     
     for i in range(wormCount - 1):
         for w in wormHoles:
@@ -15,7 +12,6 @@ for c in range(cases):
     for w in wormHoles:
         if dist[w.source-1] + w.cost < dist[w.dest-1]:
             print "UNSAFE AT ANY SPEED"
-            raise SystemExit
-
-    print " ".join([str(x) if x != 9e9 else "OUT" for x in dist])
-
+            break
+    else:
+        print " ".join([str(x) if x != 9e9 else "OUT" for x in dist])
